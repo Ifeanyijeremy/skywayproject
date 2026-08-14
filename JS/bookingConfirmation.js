b4a09cba52e9717f536ee84c69e6e4516899396e
Ifeanyi Jerry <ifeanyijerry99@gmail.com>
2026-08-13 19:32:12 -0700

    new update on the confirmation page

--- JS/bookingConfirmation.js
+++ JS/bookingConfirmation.js
@@ -38,3 +38,51 @@ downloadTicket.addEventListener("click", (event) => {
     }, 1200);
 
 });
+
+const mobileMenuBtn =
+    document.getElementById("mobileMenuBtn");
+
+const closeMenuBtn =
+    document.getElementById("closeMenuBtn");
+
+const mobileSidebar =
+    document.getElementById("mobileSidebar");
+
+const menuOverlay =
+    document.getElementById("menuOverlay");
+
+
+function openMobileMenu() {
+
+    mobileSidebar.classList.add("show");
+    menuOverlay.classList.add("show");
+
+    document.body.style.overflow = "hidden";
+}
+
+
+function closeMobileMenu() {
+
+    mobileSidebar.classList.remove("show");
+    menuOverlay.classList.remove("show");
+
+    document.body.style.overflow = "";
+}
+
+
+mobileMenuBtn.addEventListener(
+    "click",
+    openMobileMenu
+);
+
+
+closeMenuBtn.addEventListener(
+    "click",
+    closeMobileMenu
+);
+
+
+menuOverlay.addEventListener(
+    "click",
+    closeMobileMenu
+);
